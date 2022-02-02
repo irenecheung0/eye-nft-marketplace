0cc5d9167df31977cda318de447750676cd29b37
irenecheung0 <irenecheung0812@gmail.com>
2022-02-02 22:00:31 +0800

    added get image function

---
+++ getimages.js
@@ -0,0 +1,18 @@
+// get image from folder
+// create image array
+// show the most recent  x number of images 
+
+
+$(document).ready(function() { 
+    // your code here
+
+
+
+    
+});
+
+//find total no. of images
+// 
+
+//function to get total number of images in the folder
+
